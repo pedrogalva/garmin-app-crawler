414d39a2495700d1d86adbad4f1d671d349780c9
Galvani <galvanipedro11@gmail.com>
2025-01-12 17:16:17 -0300

    stopped for a while. garmin apparently blocks crawlers

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,28 @@ import { sleep } from "./utils";
 const app = async () => {
   let driver = new SeleniumDriver();
   await driver.init();
-  await driver.getUrl("https://www.google.com");
+  await garminAppLogin(driver);
+};
+
+const garminAppLogin = async (driver: SeleniumDriver) => {
+  driver.getUrl("https://connect.garmin.com/modern/");
+
+  if (
+    (await driver.getCurrentUrl()).includes(
+      "https://connect.garmin.com/modern/home"
+    )
+  ) {
+    return true;
+  }
+
+  await driver.sendKeys("galvanipedro11@gmail.com", false, '//*[@id="email"]');
+
+  await driver.sendKeys("V!v!guita123", false, '//*[@id="password"]');
+
+  await driver.clickElement(
+    false,
+    '//*[@id="portal"]/div[2]/div/div/div/div/form/section[2]/g-button/button'
+  );
 };
 
 export default app;
--- src/selenium/driver.ts
+++ src/selenium/driver.ts
@@ -1,9 +1,24 @@
-import { Builder, Browser, WebDriver, By } from "selenium-webdriver";
+import {
+  Builder,
+  Browser,
+  WebDriver,
+  By,
+  WebElement,
+} from "selenium-webdriver";
+
+import { sleep } from "../utils";
 
 export class SeleniumDriver {
   #driver!: WebDriver;
+  #defaultDelayMs: number;
 
-  constructor() {}
+  constructor(defaultDelayMs: number = 1000) {
+    this.#defaultDelayMs = defaultDelayMs;
+  }
+
+  private async delayUse(delayMs: number) {
+    await sleep(delayMs);
+  }
 
   public async init() {
     this.#driver = await this.createSeleniumService();
@@ -13,20 +28,57 @@ export class SeleniumDriver {
     return await new Builder().forBrowser(Browser.CHROME).build();
   }
 
-  public async getUrl(url: string) {
+  public async getUrl(url: string, customDelay = this.#defaultDelayMs) {
+    await this.delayUse(customDelay);
+
     await this.#driver.get(url);
   }
 
-  public async findElementXPath(xPath: string) {
+  public async findElementXPath(
+    xPath: string,
+    customDelay = this.#defaultDelayMs
+  ) {
+    await this.delayUse(customDelay);
+
     return await this.#driver.findElement(By.xpath(xPath));
   }
 
-  public async clickElementXPath(xPath: string) {
-    let element = await this.findElementXPath(xPath);
+  public async getPageHtml(customDelay = this.#defaultDelayMs) {
+    await this.delayUse(customDelay);
+
+    return await this.#driver.getPageSource();
+  }
+
+  public async getCurrentUrl(customDelay = this.#defaultDelayMs) {
+    await this.delayUse(customDelay);
+
+    return await this.#driver.getCurrentUrl();
+  }
+
+  public async clickElement(
+    element: WebElement | false,
+    xPath = "",
+    customDelay = this.#defaultDelayMs
+  ) {
+    await this.delayUse(customDelay);
+
+    if (element === false) {
+      element = await this.findElementXPath(xPath);
+    }
     await element.click();
   }
 
-  public async getPageHtml() {
-    return await this.#driver.getPageSource();
+  public async sendKeys(
+    keys: string,
+    element: WebElement | false,
+    xPath = "",
+    customDelay = this.#defaultDelayMs
+  ) {
+    await this.delayUse(customDelay);
+
+    if (element === false) {
+      element = await this.findElementXPath(xPath);
+    }
+    await element.sendKeys(keys);
   }
 }
